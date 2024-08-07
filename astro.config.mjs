e3ba63962e1c3c4311179a3707e0bf18572aafc8
Michael Toker <49562866+tokeron@users.noreply.github.com>
2024-08-07 11:24:08 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+// import { defineConfig } from 'astro/config';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+// import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://astrofy-template.netlify.app',
+//   integrations: [mdx(), sitemap(), tailwind()]
+// });
+
+import { defineConfig } from 'astro/config'
+
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+  site: 'https://tokeron.github.io/web/',
+  base: 'web',
+})
